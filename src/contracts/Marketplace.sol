66951c018bacc841d605570a259cc5a3394ca6a8
Piero Dore Panariello <pdpanariello@wm.edu>
2022-12-26 11:46:47 -0500

    created tests and function to create a product

--- src/contracts/Marketplace.sol
+++ src/contracts/Marketplace.sol
@@ -2,8 +2,51 @@ pragma solidity ^0.5.0;
 
 contract Marketplace{
     string public name; 
+    uint public productCount = 0; //how many products exist inside the mapping 
+    mapping(uint => Product) public products; //same as a dictionary / hashtable 
+
+    struct Product {
+        uint id; 
+        string name; 
+        uint price; 
+        address owner; 
+        bool purchased; 
+    }
+
+    event ProductCreated(
+        uint id,
+        string name, 
+        uint price,
+        address owner, 
+        bool purchased
+    ); 
 
     constructor() public {
         name = "Piero's Marketplace"; 
+    }
+
+    function createProduct(string memory _name, uint _price) public {
+        // require a valid name
+        require(bytes(_name).length > 0); 
+        //require a valid price 
+        require(_price > 0 ); 
+        // incriment product count 
+        productCount ++; 
+        // create the product 
+        products[productCount] = Product(productCount, _name, _price, msg.sender, false); 
+        // trigger an event
+        // can be used to console log 
+        emit ProductCreated(productCount, _name, _price, msg.sender, false); 
+
+
+    }
+
+    function purchaseProduct(uint _id) public {
+        // fetch the product 
+        // fetch the owner 
+        // make sure the product is valid 
+        // purchase the product 
+        // trigger an event 
+
     }
 }
--- test/Marketplace.test.js
+++ test/Marketplace.test.js
@@ -1,7 +1,14 @@
+const { assert } = require("chai")
+
+
 const Marketplace = artifacts.require("./Marketplace.sol")
 // using Chai as the testing suite 
 
-contract('Marketplace', (accounts) => {
+require('chai')
+    .use(require('chai-as-promised'))
+    .should(); 
+
+contract('Marketplace', ([deployer, seller, buyer]) => {
     let marketplace 
 
     before(async() => {
@@ -22,4 +29,40 @@ contract('Marketplace', (accounts) => {
             assert.equal(name, "Piero's Marketplace")
         })
     })
+
+    describe('products', async() => {
+        let result, productCount
+        before(async() => {
+            result = await marketplace.createProduct('iPhone X', web3.utils.toWei('1', 'Ether'), {from: seller}) 
+            productCount = await marketplace.productCount()
+        })
+
+        it('creates a product', async() => {
+            //  SUCCESS 
+            // product count changed 
+            assert.equal(productCount, 1)
+            const event = result.logs[0].args 
+            assert.equal(event.id.toNumber(), productCount.toNumber(), 'id is correct')
+            assert.equal(event.name, 'iPhone X', 'name is correct')
+            assert.equal(event.price, '1000000000000000000', 'price is correct')
+            assert.equal(event.owner, seller, 'owner is correct')
+            assert.equal(event.purchased, false, "purchased is correct")
+
+            // FAILURE 
+            // product must have a name 
+            await marketplace.createProduct('', web3.utils.toWei('1000000000000000000', 'Ether'), {from: seller}).should.be.rejected
+            await marketplace.createProduct('iPhone X', web3.utils.toWei('0', 'Ether'), {from: seller}).should.be.rejected
+        })
+
+        it('lists products', async() => {
+            const product = await marketplace.products(productCount)
+            assert.equal(product.id.toNumber(), productCount.toNumber(), 'id is correct')
+            assert.equal(product.name, 'iPhone X', 'name is correct')
+            assert.equal(product.price, '1000000000000000000', 'price is correct')
+            assert.equal(product.owner, seller, 'owner is correct')
+            assert.equal(product.purchased, false, "purchased is correct")
+        })
+    })
+
+
 })
