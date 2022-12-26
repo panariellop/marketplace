a8ce776157207805758e8b520b6d76b94fe92e6d
Piero Dore Panariello <pdpanariello@wm.edu>
2022-12-26 14:05:26 -0500

    ethereum backend working and can create / buy / list items for sale

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,60 @@
 import React, { Component } from 'react';
+import Web3 from 'web3';
 import logo from '../logo.png';
 import './App.css';
 
 class App extends Component {
+
+  async componentWillMount(){
+    await this.loadWeb3()
+    await this.loadBlockchainData()
+  }
+
+  async loadWeb3(){
+
+   if(window.ethereum){
+    window.web3 = new Web3(window.ethereum) 
+    await window.ethereum.enable()
+   }
+   else if(window.web3){
+    window.web3 = new Web3(window.web3.currentProvider)
+   }
+   else {
+    window.alert("Non-ethereum browser detected. You should consider using the MetaMask extension.")
+   }
+  }
+
+  async loadBlockchainData(){
+    const accounts = await window.web3.eth.getAccounts()
+    this.setState({account: accounts[0] }) 
+  }
+
+  constructor(props){
+    super(props) 
+    this.state = {
+      account: '', 
+      productCount: 0, 
+      products: [], 
+      loading: true, 
+    }
+  }
+
   render() {
     return (
       <div>
         <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
           <a
             className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
-            target="_blank"
+            href="/"
             rel="noopener noreferrer"
           >
-            Dapp University
+            Piero's Marketplace
           </a>
+          <ul className = "navbar-nav px-3">
+            <li className = "nav-item text-nowrap d-none d-sm-none d-sm-block">
+              <small className='text-white'><span id = "account">{this.state.account}</span></small>
+            </li>
+          </ul> 
         </nav>
         <div className="container-fluid mt-5">
           <div className="row">
--- src/contracts/Marketplace.sol
+++ src/contracts/Marketplace.sol
@@ -9,7 +9,8 @@ contract Marketplace{
         uint id; 
         string name; 
         uint price; 
-        address owner; 
+        address payable owner; // need to make the owner payable to allow for 
+                                // money transferable smart contracts 
         bool purchased; 
     }
 
@@ -17,7 +18,15 @@ contract Marketplace{
         uint id,
         string name, 
         uint price,
-        address owner, 
+        address payable owner, 
+        bool purchased
+    ); 
+
+    event ProductPurchased(
+        uint id,
+        string name, 
+        uint price,
+        address payable owner, 
         bool purchased
     ); 
 
@@ -41,12 +50,28 @@ contract Marketplace{
 
     }
 
-    function purchaseProduct(uint _id) public {
-        // fetch the product 
-        // fetch the owner 
-        // make sure the product is valid 
-        // purchase the product 
-        // trigger an event 
-
+    function purchaseProduct(uint _id) public payable {
+        // Fetch the product
+        Product memory _product = products[_id];
+        // Fetch the owner
+        address payable _seller = _product.owner;
+        // Make sure the product has a valid id
+        require(_product.id > 0 && _product.id <= productCount);
+        // Require that there is enough Ether in the transaction
+        require(msg.value >= _product.price);
+        // Require that the product has not been purchased already
+        require(!_product.purchased);
+        // Require that the buyer is not the seller
+        require(_seller != msg.sender);
+        // Transfer ownership to the buyer
+        _product.owner = msg.sender;
+        // Mark as purchased
+        _product.purchased = true;
+        // Update the product
+        products[_id] = _product;
+        // Pay the seller by sending them Ether
+        address(_seller).transfer(msg.value);
+        // Trigger an event
+        emit ProductPurchased(productCount, _product.name, _product.price, msg.sender, true);
     }
 }
